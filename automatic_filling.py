f906d86f3efbdc1aa8b7b0bf1c5f17240bb2755c
len4059 <len4059@gmail.com>
2024-08-26 00:47:04 +0800

    add version discription

--- automatic_filling.py
+++ automatic_filling.py
@@ -1,3 +1,6 @@
+### This is a testing code written by Enoch
+#Version 1.0
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
