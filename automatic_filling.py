9be0769baa1c142b7ce426fa39fb51aae1ee3bec
len4059 <len4059@gmail.com>
2025-04-22 13:16:33 +0800

    v2 for network lagging issue

--- automatic_filling.py
+++ automatic_filling.py
@@ -1,9 +1,9 @@
 # This is a testing code written by Enoch
 
-# Chrome version is 128.0.6613.85
-# Chromedriver version is 128.0.6613.84
+# Chrome version is 135.0.7049.96
+# Chromedriver version is 135.0.7049.95
 
-# Version 1.0 (2024/08/26)
+# Version 2.0 (2025/04/22)
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -11,6 +11,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.common.exceptions import (
+    TimeoutException, StaleElementReferenceException,
+    ElementClickInterceptedException, JavascriptException
+)
 
 import tkinter as tk
 from tkinter import font
@@ -31,126 +36,192 @@ def on_focusout(event, entry, placeholder):
         entry.insert(0, placeholder)
         entry.config(fg='grey')
 
+RETRY_EXCEPTIONS = (
+    TimeoutException,
+    StaleElementReferenceException,
+    ElementClickInterceptedException,
+    JavascriptException
+)
+
+def do(driver, action, max_retry=10, wait_time=5):
+    for attempt in range(1, max_retry + 1):
+        try:
+            return action()
+        except RETRY_EXCEPTIONS as e:
+            if attempt == max_retry:
+                driver.refresh()
+                raise  
+            time.sleep(wait_time)  
+
+
 def submit():
     global age, date, times
     age = int(age_entry.get())
     date = date_entry.get()
     times = int(times_entry.get())
     window.destroy()
-    auto()
+    auto()         
 
 
 def auto():
     global times
-    service = Service(exctable_path = "chromedriver.exe")
+    service = Service(executable_path = "chromedriver.exe")
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=options, service=service)
+    driver.implicitly_wait(2)        
 
     survey_url = "https://survey.medallia.com/?lego-retail-lcs&store=retail_20077&lng=zh_TW&fbclid=IwY2xjawE058NleHRuA2FlbQIxMAABHb4npEtLA9viddKcuL_4cbNwmejJgOiw0vRGnE6xrUBzS2aRZo5aCas8IA_aem_Cw0cH4HFZz7j31bIGqmXMA"
 
     driver.get(survey_url)
 
     while times > 0:
+
+        driver.delete_all_cookies()
     
-        start_button = WebDriverWait(driver, 10).until(
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "buttonBegin"))
-        )
+        ).click())
 
-        start_button.click()
+        time.sleep(1.5)
 
-        age_input = WebDriverWait(driver, 10).until(
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "spl_q_lego_global_age_txt"))
-        )
+        ).send_keys(age))
 
-        age_input.send_keys(age)
+        time.sleep(1.5)
 
-        next_button = WebDriverWait(driver, 10).until(
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "buttonNext"))
-        )
+        ).click())
 
-        next_button.click()
+        time.sleep(1.5)
 
-        date_input = WebDriverWait(driver, 10).until(
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "cal_q_lego_retail_transaction_date_"))
-        )
+        ).send_keys(date))
 
-        date_input.send_keys(date)
+        time.sleep(1.5)
 
-        next_button = WebDriverWait(driver, 10).until(
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "buttonNext"))
-        )
+        ).click())
 
-        next_button.click()
+        time.sleep(1.5)
 
-        driver.execute_script("document.getElementById('onf_q_lego_global_ltr10_11').checked = true;")
+        do(driver, lambda: driver.execute_script("""
+            const el = document.getElementById(arguments[0]);
+            if (!el) { throw new Error('radio not ready'); }
+            el.checked = true;
+            el.dispatchEvent(new Event('change', {bubbles:true}));
+        """, "onf_q_lego_global_ltr10_11"))
 
-        next_button = WebDriverWait(driver, 10).until(
+        time.sleep(1.5)
+
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "buttonNext"))
-        )
+        ).click())
 
-        next_button.click()
+        time.sleep(1.5)
 
-        no_contact = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/form/div/div[2]/div[1]/fieldset/div/div/ul/li[3]/div/div/div/div[2]/span'))
-        )
+        do(driver, lambda: driver.execute_script("""
+            const xpath = arguments[0];
+            const el = document.evaluate(xpath, document, null,
+                                        XPathResult.FIRST_ORDERED_NODE_TYPE, null
+                                        ).singleNodeValue;
+            if (!el) { throw new Error('noContact span not ready'); }
+            el.scrollIntoView({block:'center'});
+            el.click();
+        """, '/html/body/div/div/form/div/div[2]/div[1]/fieldset/div/div/ul/li[3]/div/div/div/div[2]/span'))
 
-        driver.execute_script("arguments[0].click();", no_contact)
+        time.sleep(1.5)
 
-        next_button = WebDriverWait(driver, 10).until(
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "buttonNext"))
-        )
+        ).click())
 
-        next_button.click()
+        time.sleep(1.5)
 
-        willing = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/form/div/div[2]/div/fieldset/div/div/ul/li[1]/div/div/div/div[2]/span'))
-        )
+        do(driver, lambda: driver.execute_script("""
+            const xp = '/html/body/div/div/form/div/div[2]/div/fieldset/div/div/ul/li[1]/div/div/div/div[2]/span';
+            const el = document.evaluate(xp, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (!el) { throw new Error('willing span not ready'); }
+            el.scrollIntoView({block:'center'});
+            el.click();
+        """))
 
-        driver.execute_script("arguments[0].click();", willing)
+        time.sleep(1.5)
 
-        next_button = WebDriverWait(driver, 10).until(
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "buttonNext"))
-        )
+        ).click())
 
-        next_button.click()
+        time.sleep(1.5)
 
-        driver.execute_script("document.getElementById('onf_q_lego_retail_fun_in_store_sat10_11').checked = true;")
-        driver.execute_script("document.getElementById('onf_q_lego_retail_greeting_recieved_sat10_11').checked = true;")
-        driver.execute_script("document.getElementById('onf_q_lego_retail_timely_assistance_sat10_11').checked = true;")
-        driver.execute_script("document.getElementById('onf_q_lego_retail_friendliness_staff_sat10_11').checked = true;")
-        driver.execute_script("document.getElementById('onf_q_lego_retail_knowledgeable_staff_sat10_11').checked = true;")
-        driver.execute_script("document.getElementById('onf_q_lego_retail_efficient_checkout_sat10_11').checked = true;")
-        driver.execute_script("document.getElementById('onf_q_lego_retail_navigation_sat10_11').checked = true;")
-        driver.execute_script("document.getElementById('onf_q_lego_retail_product_availability_sat10_11').checked = true;")
+        SAT_IDS = [
+            'onf_q_lego_retail_fun_in_store_sat10_11',
+            'onf_q_lego_retail_greeting_recieved_sat10_11',
+            'onf_q_lego_retail_timely_assistance_sat10_11',
+            'onf_q_lego_retail_friendliness_staff_sat10_11',
+            'onf_q_lego_retail_knowledgeable_staff_sat10_11',
+            'onf_q_lego_retail_efficient_checkout_sat10_11',
+            'onf_q_lego_retail_navigation_sat10_11',
+            'onf_q_lego_retail_product_availability_sat10_11',
+        ]
 
-        next_button = WebDriverWait(driver, 10).until(
+        def check_radio_by_id(driver, rid):
+            # 等 radio 真正存在再動手
+            el = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.ID, rid))
+            )
+            driver.execute_script("""
+                arguments[0].checked = true;
+                arguments[0].dispatchEvent(new Event('change', {bubbles:true}));
+            """, el)
+
+        for rid in SAT_IDS:
+            do(driver, lambda rid=rid: check_radio_by_id(driver, rid))
+
+        time.sleep(1.5)
+
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "buttonNext"))
-        )
+        ).click())
 
-        next_button.click()
+        time.sleep(1.5)
 
-        next_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "buttonNext"))
-        )
+        do(driver, lambda: WebDriverWait(driver, 10)
+            .until(EC.element_to_be_clickable((By.ID, "buttonNext")))
+            .click())
 
-        next_button.click()
+        time.sleep(1.5)
 
-        no_willing = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/form/div/div[2]/div[1]/fieldset/div/div/ul/li[2]/div/div/div/div[2]/span'))
-        )
+        do(driver, lambda: driver.execute_script("""
+            const xp = '/html/body/div/div/form/div/div[2]/div[1]/fieldset/div/div/ul/li[2]/div/div/div/div[2]/span';
+            const el = document.evaluate(xp, document, null,
+                                        XPathResult.FIRST_ORDERED_NODE_TYPE, null
+                                        ).singleNodeValue;
+            if (!el) { throw new Error('no‑willing span not ready'); }
+            el.scrollIntoView({block:'center'});
+            el.click();
+        """))
 
-        driver.execute_script("arguments[0].click();", no_willing)
+        time.sleep(1.5)
 
-        finish = WebDriverWait(driver, 10).until(
+        do(driver, lambda: WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.ID, "buttonFinish"))
-        )
+        ).click())
 
-        finish.click()
+        time.sleep(1.5)
 
-        time.sleep(1)
+        do(driver, lambda: driver.execute_script(
+            "window.location.href = arguments[0];", survey_url
+        ))
 
-        driver.execute_script("window.location.href='https://survey.medallia.com/?lego-retail-lcs&store=retail_20077&lng=zh_TW&fbclid=IwY2xjawE058NleHRuA2FlbQIxMAABHb4npEtLA9viddKcuL_4cbNwmejJgOiw0vRGnE6xrUBzS2aRZo5aCas8IA_aem_Cw0cH4HFZz7j31bIGqmXMA';")
+        time.sleep(2)
+
+        do(driver, lambda: WebDriverWait(driver, 15).until(
+            EC.element_to_be_clickable((By.ID, "buttonBegin"))))
 
         times -= 1
 
